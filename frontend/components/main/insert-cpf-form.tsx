0b646276c142082e564179680b641c996cd48be0
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-12-03 13:14:36 -0300

    feat: integração de máscara de cpf na validação

--- frontend/components/main/insert-cpf-form.tsx
+++ frontend/components/main/insert-cpf-form.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { siteConfig } from '@/config/site';
+import { validateCPF } from '@/lib/utils';
 
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import InputMask from 'react-input-mask';
 
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
@@ -31,7 +33,9 @@ const formSchema = z.object({
     .string({
       required_error: 'Precisamos de um CPF válido.',
     })
-    .length(11, 'Precisamos de um CPF válido.'),
+    .refine((value) => validateCPF(value), {
+      message: 'Precisamos de um CPF válido.',
+    }),
 });
 
 export function InsertCPFForm() {
@@ -66,7 +70,19 @@ export function InsertCPFForm() {
                       <FormItem>
                         <FormLabel htmlFor="cpf">CPF</FormLabel>
                         <FormControl>
-                          <Input id="cpf" {...field} />
+                          {/* <Input
+                            id="cpf"
+                            {...field}
+                            placeholder="123.456.789-00"
+                            data-mask="000.000.000-00"
+                          /> */}
+                          <InputMask
+                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            mask="999.999.999-99"
+                            maskChar={null}
+                            value={form.getValues('cpf')}
+                            onChange={(e) => form.setValue('cpf', e.target.value)}
+                          />
                         </FormControl>
                         <FormDescription>Digite o seu CPF</FormDescription>
                         <FormMessage />
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,34 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function validateCPF(value: string) {
+  const cleanedCPF = value.replace(/[^\d]/g, '');
+
+  if (
+    cleanedCPF.length !== 11 ||
+    /^(.)\1+$/.test(cleanedCPF) ||
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].some(
+      (i) => cleanedCPF.substring(9, 1) == i.toString().repeat(1)
+    )
+  ) {
+    return false;
+  }
+
+  let add = 0;
+  for (let i = 0; i < 9; i++) add += parseInt(cleanedCPF.charAt(i)) * (10 - i);
+  let rev = 11 - (add % 11);
+  if (rev === 10 || rev === 11) rev = 0;
+  if (rev !== parseInt(cleanedCPF.charAt(9))) return false;
+
+  add = 0;
+  for (let i = 0; i < 10; i++) add += parseInt(cleanedCPF.charAt(i)) * (11 - i);
+  rev = 11 - (add % 11);
+  if (rev === 10 || rev === 11) rev = 0;
+  if (rev !== parseInt(cleanedCPF.charAt(10))) return false;
+
+  return true;
 }
