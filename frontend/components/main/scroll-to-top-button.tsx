5a94506063303626fa529a58f6713b2abb3de703
Luan Menezes <luanmenezesmluiz@gmail.com>
2023-12-05 14:38:23 -0300

    chore: finalizado o componente scroll-to-top-button

--- frontend/components/main/scroll-to-top-button.tsx
+++ frontend/components/main/scroll-to-top-button.tsx
@@ -28,21 +28,11 @@ export function ScrollToTopButton() {
   return (
     <button
       className={`fixed bottom-4 right-4 rounded-full p-2 outline-none transition-opacity duration-200 dark:text-black dark:bg-white text-white bg-black ${
-        isVisible ? "opacity-100" : "opacity-0"
+        isVisible ? 'opacity-100' : 'opacity-0'
       }`}
       onClick={scrollToTop}
     >
       <Icons.arrowUp className="h-5 w-5" />
     </button>
-
-    /* <div className="fixed bottom-4 right-4 z-50">
-      <button
-        type="button"
-        className="p-3 rounded-full dark:text-black dark:bg-white text-white bg-black shadow-lg focus:outline-none"
-        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-      >
-        <Icons.arrowUp className="h-5 w-5" />
-      </button>
-    </div> */
   );
 }
